8220bdf4cc8b29b2279c28b261c10749870b9c5a
Tommy Son <tommy.m.son@gmail.com>
2024-06-19 19:26:28 -0700

    Add Zod validation schema to user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,13 +6,15 @@ import {
   getUsers,
   updateUser,
 } from "../controllers/user.controller";
+import { validationSchema } from "../middlewares/validation.middleware";
+import { createUserSchema, updateUserSchema } from "../schemas/user.schema";
 
 const userRoutes = Router();
 
-userRoutes.post("", createUser);
+userRoutes.post("", validationSchema(createUserSchema), createUser);
 userRoutes.get("", getUsers);
 userRoutes.get("/:userid", getUser);
 userRoutes.delete("/:userid", deleteUser);
-userRoutes.patch("/:userid", updateUser);
+userRoutes.patch("/:userid", validationSchema(updateUserSchema), updateUser);
 
 export default userRoutes;
